dd2020bca1af2aff331175f14393ed49baa49a75
Riyan First Tiyanto <riyanfirst13@gmail.com>
2022-03-25 22:15:31 +0700

    aplikasi flutter selamat datang

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+// stl enter untuk membuat myApp class
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Dasar Flutter")),
+        body: Center(child: Text('Selamat Datang !!!')),
+      ),
+    );
+  }
+}
