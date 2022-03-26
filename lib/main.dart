1f2cd10b3d90273cf5e43df88f8a13d3d8b86f71
Riyan First Tiyanto <riyanfirst13@gmail.com>
2022-03-26 11:37:02 +0700

    Text Widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Dasar Flutter")),
-        body: Center(child: Text('Selamat Datang !!!')),
-      ),
-    );
+        home: Scaffold(
+      appBar: AppBar(title: Text("Dasar Flutter")),
+      body: Center(
+          child: Container(
+              color: Colors.lightBlue,
+              width: 150,
+              height: 50,
+              child: Text(
+                'Latihan untuk Flutter, dengan belajar dasar dasarnya',
+                textAlign: TextAlign.center,
+                style:
+                    TextStyle(color: Colors.white, fontStyle: FontStyle.italic),
+              )))
+    ));
   }
 }
